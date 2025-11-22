c751119679404c80b7342309a3110b056d3923ff
redstringoffate <redstringoffate@naver.com>
2025-11-22 10:59:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ import io
 import re
 from openpyxl import Workbook
 from openpyxl.styles import Font, Alignment
+import sys
+import os
 
-from calculations.arudha_calc import calc_all_arudhas
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CALC_PATH = os.path.join(BASE_DIR, "calculations")
+sys.path.append(CALC_PATH)
+
+from arudha_calc import calc_all_arudhas
 from calculations.ul_calc import calc_UL
 from data.houses import generate_house_lords, generate_house_signs
 
@@ -215,4 +221,5 @@ elif st.session_state.page == "main_results":
 
 elif st.session_state.page == "interpret":
     show_interpretation_screen()
+
 
